c733a05e48ef9ff04d8cac591fe30ddb577ef9cc
Damiem Mclaurin <damiemmclaurin@Damiems-MacBook-Pro.local>
2023-06-05 23:27:35 -0400

    ADDED structure to the code and added html routes and listining.

--- server.js
+++ server.js
@@ -15,12 +15,36 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+//Static Middleware
 app.use(express.static("./develop/public"));
 
+
+//Api Route / GET 
 app.get("/api/notes", function(req,res) {
     readFileAsync("./develop/db/db.json", "uft8").then(function(data) {
         notes = [].concat(JSON.parse(data))
         res.json(notes);
     })
+});
+
+// HTML
+
+app.get("/notes", function(req,res) {
+    res.sendFile(path.joint(__dirname, "./develop/public/notes.html"));
+});
+
+app.get("/", function(req,res) {
+    res.sendFile(path.joint(__dirname, "./develop/public/index.html"));
+});
+
+app.get("*", function(req,res) {
+    res.sendFile(path.joint(__dirname, "./develop/public/index.html"));
+});
+
+
+
+
+//Listen
+app.listen(PORT, function() {
+    console.log("App Listening on PORT " + PORT);
 });
