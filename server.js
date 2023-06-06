cb7a21cf01507f2c91231f57bd66d1f082845ab6
Damiem Mclaurin <damiemmclaurin@Damiems-MacBook-Pro.local>
2023-06-05 23:28:33 -0400

    added more structure to code.

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-
+//
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
 const util = require("util");
 
-
+//Processes
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 
+//Setting The Server Up
 const app = express();
 const PORT = process.env.PORT || 8000;
 
